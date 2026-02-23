24114753f54f336b14529533f4e75e155a5162e4
Filip Simeonov <fsimeonov@apple.com>
2026-02-23 21:28:07 +0100

    Fix for submit contact button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import "./Button.scss";
 
-export default function Button({ title, fontSize, onClick, width }) {
+export default function Button({ title, fontSize, onClick, width, type = "button" }) {
+  const handleClick = (e) => {
+    if (type === "submit") {
+      const form = e.target.closest("form");
+      if (form) form.requestSubmit();
+    }
+    if (onClick) onClick(e);
+  };
+
   return (
-    <div className="button" style={{ fontSize, width, marginTop: "1rem" }} onClick={onClick}>
+    <div className="button" style={{ fontSize, width, marginTop: "1rem" }} onClick={handleClick}>
       {title}
     </div>
   );
-}
+}s
